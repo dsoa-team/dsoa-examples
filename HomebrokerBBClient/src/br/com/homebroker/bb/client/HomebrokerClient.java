381aefc9211e3f606a7f79be23fefc0c5fe8c5a7
Tarcisio Coutinho <tcs5cin@gmail.com>
2013-10-29 19:19:39 -0200

    invoke with threads

--- HomebrokerBBClient/src/br/com/homebroker/bb/client/HomebrokerClient.java
+++ HomebrokerBBClient/src/br/com/homebroker/bb/client/HomebrokerClient.java
@@ -7,26 +7,48 @@ import org.osgi.framework.BundleContext;
 import br.com.bb.homebroker.Homebroker;
 import br.com.bb.stock.Stock;
 
-public class HomebrokerClient {
+public class HomebrokerClient implements Runnable {
 
 	private Homebroker homebroker;
-
+	Thread invocations;
+	boolean started = false;
+	
 	public HomebrokerClient(BundleContext ctx) throws SecurityException, IOException {
 		System.out.println("#### Constructor");
+		invocations = new Thread(this);
 	}
 
 	public void start() throws InterruptedException {
-		System.out.println("============================ PRICE ALERT ========================================");
-		
-		for (int i = 1; i < 10000; i++) {
-			System.out.print("priceAlert " + i + " = ");
-			System.out.println(homebroker.priceAlert("ENDERECO", Stock.PETR3, 0, 1000));
-			Thread.sleep(1000);
+		System.out.println("==>> STARTING...");
+		if(!started){
+			invocations.start();
+			started = true;
 		}
-		System.out.println("==================================================================================");
 	}
 
 	public void stop() {
 		System.out.println("==>> STOPPING...");
+	}
+
+	@Override
+	public void run() {
+
+		System.out.println("===================== PRICE ALERT ==================================");
+
+		for (int i = 1; i < 50; i++) {
+			System.out.print("priceAlert " + i + " = ");
+			try {
+				System.out.println(homebroker.priceAlert("ENDERECO", Stock.PETR3, 0, 1000));
+			} catch (Exception e) {
+				System.out.println("INVALID INVOCATION");
+			}
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("================================================================");
+
 	}
 }
