d8176e25bc6cb3da5fcddee0d5d73c12117f8fb5
Tarcisio Coutinho <tcs5cin@gmail.com>
2014-03-28 18:23:23 -0300

    update sample

--- HomebrokerBBClient/src/br/com/homebroker/bb/client/HomebrokerClient.java
+++ HomebrokerBBClient/src/br/com/homebroker/bb/client/HomebrokerClient.java
@@ -1,6 +1,12 @@
 package br.com.homebroker.bb.client;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 import org.osgi.framework.BundleContext;
 
@@ -13,9 +19,39 @@ public class HomebrokerClient implements Runnable {
 	private Homebroker homebroker;
 	Thread invocations;
 	boolean started = false;
+	
+	private long time = 0;
+	private Logger invocation;
+	private FileHandler invocation_f;
 
 	public HomebrokerClient(BundleContext ctx) throws SecurityException, IOException {
 		System.out.println("#### Constructor");
+		
+java.util.logging.Formatter f = new java.util.logging.Formatter() {
+			
+			private final DateFormat df = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss.SSS");
+
+			public String format(LogRecord record) {
+				StringBuilder builder = new StringBuilder(1000);
+				builder.append(formatMessage(record));
+				builder.append("\n");
+				return builder.toString();
+			}
+		};
+		
+		invocation = Logger.getLogger("InvocationReal");
+		
+		try {
+			invocation_f = new FileHandler("log_invocation_real.txt");
+			invocation_f.setFormatter(f);
+			invocation.addHandler(invocation_f);
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
 		invocations = new Thread(this);
 	}
 
@@ -42,11 +78,16 @@ public class HomebrokerClient implements Runnable {
 		while (request) {
 			try {
 				count++;
+				long req = System.currentTimeMillis();
 				homebroker.priceAlert("ENDERECO", Stock.PETR3, 0, 1000);
+				long resp = System.currentTimeMillis();
+				invocation.log(Level.INFO, now() +"," +  (resp-req));
 			} catch (OutOfScheduleException e) {
 				businessExceptions ++;
+				invocation.log(Level.INFO, now() +", 0");
 			} catch (Exception e) {
 				avaiExcept ++;
+				invocation.log(Level.INFO, now() +", 0");
 			}
 			long time = System.currentTimeMillis();
 			if (time - start >= 120000) {
@@ -63,4 +104,18 @@ public class HomebrokerClient implements Runnable {
 		System.err.println("Total bussinesException: " + businessExceptions);
 		System.err.println("Total availabilityException: " + avaiExcept);
 	}
+	
+private String now(){
+		
+		if(time == 0){
+			time = System.currentTimeMillis();
+			return "0";
+		}
+		
+		long current = System.currentTimeMillis();
+		String now = (current - time)+"";
+
+		return now;
+	}
+
 }
